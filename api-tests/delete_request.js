eafeb62ec2711c91ebc9e5884da43ebe6a6b7844
Anna Mayboroda <mayboroda1985@gmail.com>
2023-02-17 06:48:04 -0500

    updated delete request

--- api-tests/delete_request.js
+++ api-tests/delete_request.js
@@ -1,22 +1,21 @@
 const axios = require("axios");
 const { expect } = require("chai");
 
-describe("DELETE API Request Tests", async () => {
-    let userId;
-    before(async () => {
-        // Get a list of users sorted by creation date
-        const res = await axios.get('https://reqres.in/api/users?page=2');
-    
-        // Extract the userId of the last created user from the response
-        const lastUser = res.data[res.data.length - 1];
-       userId = lastUser.id;
-        console.log(lastUser);
-        console.log(userId);
-      });
-    
-    it("should be able delete user with id 178", async () => {
-        const res = await axios.delete('https://reqres.in/api/users/${userId}');
-        console.log(res.data);
-        expect(res.status).equal(204);
-    })
-})
+describe("DELETE API Request Tests", () => {
+
+  let lastUserId;
+
+  before(async () => {
+    // Get a list of users sorted by creation date
+    const res = await axios.get('https://reqres.in/api/users?page=2');
+    const responseArray = res.data.data;
+    lastUserId = responseArray[responseArray.length - 1].id;
+    console.log(lastUserId);
+  });
+
+  it('should delete the last user', async () => {
+    const res = await axios.delete('https://reqres.in/api/users/${lastUserId}');
+    expect(res.status).to.equal(204);
+  });
+
+});
